1a97a41acb066c5be4bd9aeb7ee06b7cf77cbf2e
qitas <qitas@qitas.cn>
2019-08-16 11:31:10 +0800

    fix

--- src/cc1101_esp_pin_config.h
+++ src/cc1101_esp_pin_config.h
@@ -5,7 +5,7 @@
 #if defined ESP32
   #define CC1101_GDO0 34   // Use GPIO PIN 34 as the interrupt pin
 #elif defined ESP8266
-  #define CC1101_GDO0 16   // Use GPIO PIN D2 as the interrupt pin
+  #define CC1101_GDO0 4   // Use GPIO PIN D2 as the interrupt pin
 			   // Can't use D0 as it doesn't support Interrupts
 			   // Can't use D3 and D4 as the pin is pulled HIGH which the CC1101
 			   // pulls LOW - connected to FLASH button, boot fails if pulled LOW
